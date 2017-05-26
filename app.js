d0c2ef0547c845267852a850ecf2210688f8867d
nandym <nandym1233@gmail.com>
2017-05-26 22:01:04 +0100

    version 1.0.5

--- app.js
+++ app.js
@@ -3,27 +3,17 @@ var app = express();
 var serv = require("http").Server(app);
 //requires the socket used for communication client<->server (Goes both ways)
 var io = require("socket.io")(serv,{});
-
 app.set('port', (process.env.PORT || 5000));
-
 app.get('/', function(request, response) {
-  response.render('client/index.html');
+  response.sendFile(__dirname+"/client/index.html");
 });
-
-
-
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 console.log("Gives me the information that the server has started.")
 /*
-
 */
-
 var socket_Players = {};
-
-
 /*
 hehehe
 >:(
@@ -31,7 +21,6 @@ hehehe
 indeed
 Success!
 */
-
 io.sockets.on('connection', function(socket){
 //nice stuff like stats eh
   socket.id =Math.floor(Math.random()*10000);
@@ -51,7 +40,6 @@ io.sockets.on('connection', function(socket){
 
     });
 });
-
 setInterval(function(){
 var pack = [];
   for(var i  in socket_Players){
