4937835eddc01e984e346c072038a0e3f4a673b2
nandym <nandy_merkens@sapo.pt>
2017-05-26 15:14:47 +0100

    Do changing it

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Hello world");
+var express = require("express");
+var app = express();
+var serv = require("http").Server(app);
+//requires the socket used for communication client<->server (Goes both ways)
+var io = require("socket.io")(serv,{});
+
+
+/*
+app.get("/",function(req, res){
+res.sendFile(__dirname+"/client/img/smiley-face.jpg");
+});
+app.use("/client/",express.static(__dirname+"/client"));
+
+
+*/
+
+console.log("Gives me the information that the server has started.")
+serv.listen(2000);
+io.sockets.on("connection",function(socket) {
+  console.log("Tells me a client connected.")
+})
